bc7700dc0f24c6144c6ae9681090bfb78f50cc0d
[Niggle Nandu] <nigglenandu@gmail.com>
2024-08-28 10:43:10 -0700

    schema.sql added

---
+++ spring-boot-example/src/main/resources/schema.sql
@@ -0,0 +1,18 @@
+-- create database springbootsecurityjwt;
+-- use springbootsecurityjwt;
+--
+-- CREATE TABLE users (
+--                        username VARCHAR(50) COLLATE utf8mb4_unicode_ci NOT NULL PRIMARY KEY,
+--                        password VARCHAR(500) COLLATE utf8mb4_unicode_ci NOT NULL,
+--                        enabled BOOLEAN NOT NULL
+-- );
+--
+-- -- Create authorities table
+-- CREATE TABLE authorities (
+--                              username VARCHAR(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+--                              authority VARCHAR(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+--                              CONSTRAINT fk_authorities_users FOREIGN KEY (username) REFERENCES users(username),
+--                              PRIMARY KEY (username, authority)
+-- );
+--
+-- CREATE UNIQUE INDEX ix_auth_username ON authorities (username, authority);
